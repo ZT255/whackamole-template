b49582a3425ae9333aa8fbb046e4c15e3d96442a
ZT255 <ztawam@hotmail.com>
2024-11-18 23:18:13 -0500

    Implemented the mole being placed in a random position upon being pressed

--- whackamole.py
+++ whackamole.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 
 def main():
@@ -7,16 +8,27 @@ def main():
         # You can draw the mole with this snippet:
         mole_image = pygame.image.load("mole.png")
         screen = pygame.display.set_mode((640, 512))
-        screen.blit(mole_image, mole_image.get_rect(topleft=(x, y)))
         clock = pygame.time.Clock()
         running = True
+        x, y = 0, 0
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    pos = event.pos
+                    if pos[0] in range(x, x + 32) and pos[1] in range(y, y + 32):
+                        x = 32 * (random.randrange(0, 640) // 32)
+                        y = 32 * (random.randrange(0, 512) // 32)
             screen.fill("light green")
+            for i in range(20):
+                pygame.draw.line(screen, "dark blue", (32 + (32*i), 0), (32 + (32*i), 512))
+            for i in range(16):
+                pygame.draw.line(screen, "dark blue", (0, 32 + (32 * i)), (640, 32 + (32 * i)))
+            screen.blit(mole_image, mole_image.get_rect(topleft=(x, y)))
             pygame.display.flip()
             clock.tick(60)
+
     finally:
         pygame.quit()
 
